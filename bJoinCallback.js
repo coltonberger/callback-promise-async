3182fd1658c80e12a62cc8a3f87d1570060f68c2
Colton Berger <coltonberger@optonline.net>
2018-05-24 17:40:44 -0700

    added anwser to b

--- bJoinCallback.js
+++ bJoinCallback.js
@@ -1,9 +1,43 @@
 const fs = require('fs');
+const joiner = (response) => {
+  fs.readFile('./users.json', (err, data) => {
+    const users = JSON.parse(data);
+    fs.readFile('./books.json', (err, data) => {
+      const books = JSON.parse(data);
+      fs.readFile('./reviews.json', (err, data) => {
+        const reviews = JSON.parse(data);
+        const output = [];
 
-const joiner = (cb) => {
-    // You cannot use fs.readFileSync here
-    // Use https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-    cb([]);
-};
+        for (let review of reviews) {
+          for (let book of books) {
+            if (book.id !== review.bookId) {
+              continue;
+            }
+            for (let user of users) {
+              if (user.id !== review.userId) {
+                continue;
+              }
+              const joined = {
+                "name": user.firstName,
+                "book": book.title,
+                "rating": review.stars,
+                "review": review.text
+              }
+              output.push(joined);
+            }
+          }
+        }
+
+        return response(output);
+
+      })
+    })
+  })
+
+
+}
+
+
+
 
 module.exports = joiner;
