3a9098183194d156ddaa1f656a4e70124d39f0dc
Colton Berger <coltonberger@optonline.net>
2018-05-28 09:22:36 -0700

    added solution

--- cJoinPromise.js
+++ cJoinPromise.js
@@ -3,11 +3,40 @@ const { promisify } = require('util');
 const readFileAsync = promisify(fs.readFile);
 
 const joiner = () => {
-    // You cannot use fs.readFileSync here
-    // Use https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-    // and return "a promise to join the files"
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-    return Promise.resolve([]);
+  let users;
+  let books;
+  let reviews;
+  let output = [];
+
+  return readFileAsync('./users.json')
+    .then((data)=>{
+      users = JSON.parse(data);
+      return readFileAsync('./books.json');
+    }).then((data)=>{
+      books = JSON.parse(data)
+      return readFileAsync('./reviews.json')
+    }).then((data)=>{
+      reviews = JSON.parse(data);
+
+      for (let review of reviews) {
+        for (let book of books) {
+          for (let user of users) {
+
+            if (review.userId === user.id && review.bookId === book.id)  {
+
+               let joined = {
+                 "name":user.firstName,
+                 "book":book.title,
+                 "rating":review.stars,
+                 "review":review.text
+               }
+               output.push(joined);
+            }
+          }
+        }
+      }
+      return output;
+    })
 }
 
 module.exports = joiner;
--- dJoinAsync.js
+++ dJoinAsync.js
@@ -6,8 +6,31 @@ const readFileAsync = promisify(fs.readFile);
 const joiner = async () => {
     // You cannot use fs.readFileSync here
     // Use https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-    // Use the `await` operator to avoid manually returning a promise (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await)
-    return Promise.resolve([]);
-};
+    // Use the `await` operator to avoid manually returning a promise (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await) LINK DOESNT WORK!!!!
+    let output = [];
+
+    const users = JSON.parse(await readFileAsync('./users.json'));
+    const books = JSON.parse(await readFileAsync('./books.json'));
+    const reviews = JSON.parse(await readFileAsync('./reviews.json'));
+
+    for (let review of reviews) {
+      for (let book of books) {
+        for (let user of users) {
+          if (review.userId === user.id && review.bookId === book.id) {
+
+            let joined = {
+              "name":user.firstName,
+              "book":book.title,
+              "rating":review.stars,
+              "review":review.text
+            }
+            output.push(joined);
+          }
+        }
+      }
+    }
+
+    return output;
+  };
 
 module.exports = joiner;
